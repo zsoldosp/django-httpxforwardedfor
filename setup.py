f3b29e644acbee7aa1ed319a65628783d7110cbb
Martin Bauer <martin.bauer@paessler.com>
2017-01-05 03:03:01 +0100

    Remove reference to BSD license

--- setup.py
+++ setup.py
@@ -36,14 +36,13 @@ setup(
     include_package_data=True,
     install_requires=[
     ],
-    license="BSD",
+    license="",
     zip_safe=False,
     keywords='django-httpxforwardedfor',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
