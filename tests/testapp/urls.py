8ccfa09c2547d6d6950ada5274874405735ca1ad
Martin Bauer <martin.bauer@paessler.com>
2017-04-12 15:54:04 +0200

    Remove unneeded file

--- tests/testapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-
-
-urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls), {}, "admin-index"),
-    # url(r'^some-path/$', some_view, {}, 'some_view'),
-]
