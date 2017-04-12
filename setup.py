a47e9480a2d6991e7ac5bfb49448f95b07879523
Martin Bauer <martin.bauer@paessler.com>
2017-04-12 14:42:53 +0200

    output of cookiecutter

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import httpxforwardedfor
+
+from setuptools import setup
+
+version = httpxforwardedfor.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('make release')
+    sys.exit()
+
+readme = open('README.rst').read()
+
+description = "Set request.META['REMOTE_ADDR'] from request.META['HTTP_X_FORWARDED_FOR']"
+
+setup(
+    name='httpxforwardedfor',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Paessler AG',
+    author_email='bis@paessler.com',
+    url='https://github.com/PaesslerAG/django-httpxforwardedfor',
+    packages=[
+        'httpxforwardedfor',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.8,<=1.11',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django-httpxforwardedfor',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+    ],
+)
