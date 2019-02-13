3ec3cdd0a36bf947ff150aab6af1e4975ceeab08
Sebastian Bonhag <sebastian.bonhag@paessler.com>
2019-02-13 16:22:48 +0100

    Update supported versions.

--- setup.py
+++ setup.py
@@ -48,8 +48,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Django',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
     ],
 )
